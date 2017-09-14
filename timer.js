e15cb539b62ae1a696e5e6a7a7f61cb51bedbed7
BakerShoeMaker <info@quackenworth.com>
2017-09-14 14:55:10 -0700

    intial work on design

--- timer.js
+++ timer.js
@@ -1,1 +1,15 @@
 
+//ADD TIMER: user selects the 'add timer' button (modal popup?)
+    //user adds name of timer (has a default name).
+    // user selects from radio button (work time or break).
+    //user selects time from (selection menu).
+    //user can save the message.
+
+//Timer appears on the screen (name of timer, time, play/pause button, progress bar, count down time)
+    //User can start/stop timer.
+    //Timer progress bar and timer will count down.
+    //Only one timer can operate at a time.
+    //When one timer ends the other timer will start.
+
+// ADDING ANOTHER TIMER: User can add another timer
+    //Click the add timer button.
