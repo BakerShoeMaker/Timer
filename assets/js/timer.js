10e1c454a28420267de569be9a5057f996e5192b
BakerShoeMaker <info@quackenworth.com>
2017-10-16 14:38:34 -0700

    working on progress bar width

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,19 +1,100 @@
-$("#bnt_CreateTimer").on("click", function()
+var countDownTime = 5;
+var display = $('#timeTicker');
+
+$("#bnt_CreateTimer").on("click",
+
+    function()
     {
-    $("#MainArea").html("");
-    runTimerSetUp();
+        $("#MainArea").html("");
+        runTimerSetUp();
+        startTimer(countDownTime, display); //should work on click only.
     }
-
-
 
 )//end CreateButtonTimer
 
-function runTimerSetUp() {
 
-    $("#playPause").html("<img src='play.png' >");
+//******************* START ************************
+
+function pauseTime(){
+
+}
+
+function resumeTime() {
+
+}
+
+//******************* PAUSE/RESUME ******************
+
+
+function runTimerSetUp() {
+    var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $("#TimerTitle").html("<h1>Today's Schedule</h1>");
+    $("#CurentTime").html(currentTime);
+    $("#TimerInfo").html("Session 1");
+    $("#playPause").html("<img src='pause.png' >");
     $("#timeTicker").html('<p>00:00</p>');
-    $("#ProgressBar").html('<div class = "progress"> <div class="progress-bar w-25" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div> </div>');
+    $("#ProgressBar").html('<div class = "progress"> <div class="progress-bar bg-success w-50" role="progressbar"' +
+        'aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div> </div>');
+    $("#playPause").on("click", startTimer);
+
+
 };
+
+console.log($("#pb").attr("data-status"));
+
+//Starts the timer
+function startTimer(duration, display) {
+    //https://stackoverflow.com/questions/20618355/the-simplest-possible-javascript-countdown-timer
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer()
+    {
+        // get the number of seconds that have passed since startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.html("<p>"+minutes + ":" +seconds +"</p>");
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration example 05:00 not 04:59
+            start = Date.now() + 1000;
+        }
+        if(minutes == 0 && seconds == 0){
+            console.log("Times up!");
+            //Stop the timer and start the next timer (if it is available).
+        }
+
+        //update progress bar width
+        //$(ProgressBar).attr('w', 'w-'+((countDownTime - diff/countDownTime)*100));
+
+        console.log(countDownTime - diff);
+        console.log( $(ProgressBar).attr('w').val());
+
+    };
+    // we don't want to wait a full second before the timer starts
+    timer();
+    setInterval(timer, 1000);
+}
+
+
+
+
+
+
+
+
+
+//***************** TIMER START ***********************
+
+//***************** TIMER END ***********************
 
 
 //ADD TIMER: user selects the 'add timer' button (modal popup?)
